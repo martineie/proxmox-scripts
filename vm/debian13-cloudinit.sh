43e4ba5f9e94bccc0947f14dbb8f03751ccd8930
Martin Eie <martin.e.eie@gmail.com>
2026-04-12 00:34:23 +0200

    Syntax fixes

--- vm/debian13-cloudinit.sh
+++ vm/debian13-cloudinit.sh
@@ -61,7 +61,7 @@ qm create $VM_ID --name $VM_NAME --net0 virtio,bridge=$BRIDGE --scsihw $SCSI_CON
 qm importdisk $VM_ID $IMAGE_NAME $STORAGE_NAME
 
 # Configure VM hardware
-qm set $VM_ID --efidisk0 $STORAGE_NAME:4 --scsi0 $STORAGE_NAME:0,size=$DISK_SIZE --ide2 local:cloudinit --boot order=scsi0 --serial0 socket --agent enabled=$AGENT_ENABLE --cicustom user=local:snippets/user-data-$VM_ID.yaml
+qm set $VM_ID --efidisk0 $STORAGE_NAME:4M --scsi0 $STORAGE_NAME,size=$DISK_SIZE --ide2 local:cloudinit --boot order=scsi0 --serial0 socket --agent enabled=$AGENT_ENABLE --cicustom user=local:snippets/user-data-$VM_ID.yaml
 
 # Regenerate cloud-init ISO with custom data
 qm cloudinit update $VM_ID
