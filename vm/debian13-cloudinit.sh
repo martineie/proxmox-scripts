dfcb22990c9dd58bfb9cec61eb26c7d5a080165c
Martin Eie <martin.e.eie@gmail.com>
2026-04-12 00:18:42 +0200

    Misc. changes

--- vm/debian13-cloudinit.sh
+++ vm/debian13-cloudinit.sh
@@ -45,9 +45,6 @@ pushd $TEMP_DIR >/dev/null
 # Download the image
 wget $IMAGE_URL -O $IMAGE_NAME
 
-# Download user-data.yaml from GitHub
-wget https://raw.githubusercontent.com/martineie/proxmox-scripts/main/vm/user-data.yaml -O user-data.yaml
-
 # Download user-data.yaml directly to snippets directory
 SNIPPETS_DIR="/var/lib/vz/snippets"
 mkdir -p $SNIPPETS_DIR  # Ensure directory exists
@@ -63,7 +60,7 @@ qm create $VM_ID --name $VM_NAME --net0 virtio,bridge=$BRIDGE --scsihw $SCSI_CON
 qm importdisk $VM_ID $IMAGE_NAME $STORAGE_NAME
 
 # Configure VM hardware
-qm set $VM_ID --efidisk0 $STORAGE_NAME:4 --scsi0 $STORAGE_NAME:0,size=$DISK_SIZE --ide2 $STORAGE_NAME:cloudinit --boot order=scsi0 --serial0 socket --agent enabled=$AGENT_ENABLE --cicustom user=local:/var/lib/vz/snippets/user-data-$VM_ID.yaml
+qm set $VM_ID --efidisk0 $STORAGE_NAME:4 --scsi0 $STORAGE_NAME:0,size=$DISK_SIZE --ide2 $STORAGE_NAME:cloudinit --boot order=scsi0 --serial0 socket --agent enabled=$AGENT_ENABLE --cicustom user=local:snippets/user-data-$VM_ID.yaml
 
 # Regenerate cloud-init ISO with custom data
 qm cloudinit update $VM_ID
